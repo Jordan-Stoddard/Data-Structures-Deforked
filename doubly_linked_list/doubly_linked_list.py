18d905ab202bd45478c5b5f2b5e51eb508fd5d50
Jordan Stoddard <jjsincorporated@gmail.com>
2019-06-11 16:57:21 -0700

    add comments for future self

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -63,9 +63,10 @@ class DoublyLinkedList:
 
 
   def remove_from_head(self):
+    # If the list is empty
     if not self.head and not self.tail:
       return None
-    
+    # If the list only has one item in it.
     elif self.head is self.tail:
         old_head = self.head
         self.head = None
@@ -80,12 +81,12 @@ class DoublyLinkedList:
 
   def add_to_tail(self, value):
     new_node = ListNode(value)
-
+    # if the list is empty
     if not self.head and not self.tail:
         self.head = new_node
         self.tail = new_node
         self.length += 1
-
+    # if the list only has one item
     elif self.head is self.tail:
         self.tail = new_node
         self.tail.prev = self.head
@@ -99,16 +100,16 @@ class DoublyLinkedList:
         self.length += 1
 
   def remove_from_tail(self):
+    #if the list is empty
     if not self.head and not self.tail:
         return None
-    
+    # if the list only has one item
     elif self.head is self.tail:
         old_tail = self.tail
         self.head = None
         self.tail = None
         self.length -= 1
         return old_tail.value
-    
     else:
         old_prev = self.tail.prev
         self.tail = old_prev
@@ -116,8 +117,10 @@ class DoublyLinkedList:
         self.tail.next = None
 
   def move_to_front(self, node):
+    # If the node put in is already the head node.
     if self.head == node:
       return
+      # if there's only one node in the list.
     elif node.prev == self.head and node == self.tail:
       old_head = self.head
       self.head = node
@@ -141,8 +144,10 @@ class DoublyLinkedList:
       old_head.prev = self.head
 
   def move_to_end(self, node):
+    # if the node put in is already the tail node.
     if self.tail == node:
       return
+    # if there's only one item in the list
     elif node.next == self.tail and node == self.head:
       old_tail = self.tail
       self.tail = node
@@ -166,16 +171,26 @@ class DoublyLinkedList:
       old_tail.next = self.tail
 
   def delete(self, node):
+    #If there's only one item in the list.
     if node.next == None and node.prev == None:
       self.head = None
       self.tail = None
       self.length -= 1
+      # If the node selected is the head node.
     elif self.head == node:
       old_head = self.head
       self.head = old_head.next
       self.head.next = old_head.next.next
       self.head.prev = None
       self.length -= 1
+      # if the node selected is the tail node.
+    elif self.tail == node:
+      old_tail = self.tail
+      self.tail = old_head.prev
+      self.tail.prev = old_tail.prev.prev
+      self.tail.next = None
+      self.length -= 1
+      # if there's only one item in the list.
     elif self.head.next == self.tail and self.tail.prev == self.head:
       self.head = node
       self.head.next = None
@@ -191,12 +206,19 @@ class DoublyLinkedList:
       self.length -= 1
     
   def get_max(self):
+    # If the list is empty return None.
     if not self.head and not self.tail:
       return None
+      # Set the current node
     current = self.head
+      # set the current max value
     current_max = self.head.value
+          #While node does not == None
     while current:
+      # if the current value is greater than the current max
+      # set the current max to be the current value and iterate to the next node
       if current.value > current_max:
         current_max = current.value
       current = current.next
+      # Once we get to the end of the DLL, return current_max
     return current_max
