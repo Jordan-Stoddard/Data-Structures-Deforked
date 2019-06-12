4dec6dd16be554c4d33b2c48e98f5f4dc7d8907f
Jordan Stoddard <jjsincorporated@gmail.com>
2019-06-12 11:03:06 -0700

    guided demo version of max_heap

--- heap/max_heap.py
+++ heap/max_heap.py
@@ -15,7 +15,23 @@ class Heap:
     pass
 
   def _bubble_up(self, index):
-    pass
+    # keep bubbling up until we've either reached the top of the heap
+    # or we've reached a point where the parent is higher priority.
+    while index > 0:
+      # On a single bubble_up iteration:
+        # get the parent index
+        parent = (index -1 ) // 2
+        # compare the child against the value of the parent.
+        # if the child's value is higher prio than it's parents value:
+        if self.storage[index] > self.storage[parent]:
+          # swap them.
+          self.storage[index], self.storage[parent] = self.storage[parent], self.storage[index]
+          # update the child's index to be the new index it is now at.
+          index = parent
+        # otherwise, child is at a valid spot
+        else:
+          # stop bubbling
+          break
 
   def _sift_down(self, index):
     pass
